7809f0c7621e592769745a7a2ffc34dd6eb3c94b
abishekselvam <abishekselvam@github.com>
2018-05-19 14:42:11 +0530

    modalcss

--- modal.js
+++
@@ -1,7 +0,0 @@
-$("#x1").click(function(){  
-        if (confirm('Are you sure?')) {
-    alert('Login successful');
-        } else {
-    alert('You can re-enter the values');
-                }
-    });
