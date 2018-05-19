1ef0efe124418c66869c69428f708c1533829277
abishekselvam <abishekselvam@github.com>
2018-05-19 14:47:41 +0530

    sampletest

---
+++ index.js
@@ -0,0 +1,7 @@
+$("#x1").click(function(){  
+        if (confirm('Are you sure?')) {
+    alert('Login successful');
+        } else {
+    alert('You can re-enter the values');
+                }
+    });
